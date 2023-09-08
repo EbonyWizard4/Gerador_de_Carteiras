19a4dfdba9129e0189979bad41ec0a48e2ce0786
EbonyWizard4 <antonio.jhone@hotmail.com>
2023-09-08 15:37:17 -0300

    refact: Refatorar código para as regras do PEP8

--- main.py
+++ main.py
@@ -1,54 +1,56 @@
+"""Módulo Principal"""
+
 import customtkinter as ctk
 
-from tkinter import *
-from tkinter import filedialog
-
 from scraping import Scraping
-from modelo_01 import Model_01
+from modelos import Model_01
 from do_pdf.do_pdf import Do_pdf
 
 
-"""Executa os modulos do programa"""
+# """Executa os modulos do programa"""
 
 
-def Criar():
-    """Coletar dados da internet"""
-    # Scraping()
+def criar():
+    """Executa o Programa em ordem"""
+    # """Coletar dados da internet"""
+    Scraping()
 
-    """Criar carteira através do modelo"""
-    # Model_01()
+    # """Criar carteira através do modelo"""
+    Model_01()
 
-    """Salva o relatório em diretório especificado"""
-    destino = Seletor()
+    # """Salva o relatório em diretório especificado"""
+    destino = seletor()
     Do_pdf(destino)
     root.destroy()
 
 
-"""Criar Janela Principal"""
+# """Criar Janela Principal"""
 root = ctk.CTk()
 root._set_appearance_mode("dark-blue")
 root.title("Ebw Invest")
 root.geometry("400x200")
 root.resizable(width=False, height=False)
 
-"""Criando seletor"""
+# """Criando seletor"""
 
 
-def Seletor():
+def seletor():
+    """Escolhe diretório de saída"""
     destino = ctk.filedialog.askdirectory(title="Escolha onde salvar o relatório")
     return destino
 
 
-"""Criar Frame para exibir elementos Titulo da Tela"""
-frame = ctk.CTkFrame(master=root, width=390, height=190).place(x=5, y=5)
+# """Criar Frame para exibir elementos Titulo da Tela"""
+FRAME = ctk.CTkFrame(master=root, width=390, height=190).place(x=5, y=5)
 
-text = ctk.CTkLabel(
-    frame, text="Criador Automático de\nCarteiras de Investimento", font=("arial", 20)
+TEXT = ctk.CTkLabel(
+    FRAME, text="Criador Automático de\nCarteiras de Investimento", font=("arial", 20)
 )
-text.pack(pady=25)
+TEXT.pack(pady=25)
 
-btn = ctk.CTkButton(frame, text="Gerar", font=("arial", 20), command=Criar).pack(
+BTN = ctk.CTkButton(FRAME, text="Gerar", font=("arial", 20), command=criar).pack(
     pady=10
 )
 # ttk.Button(frm, text="Criar", command=Criar).grid(column=0, row=1)
-root.mainloop()
+if __name__=="__main__":
+    root.mainloop()
